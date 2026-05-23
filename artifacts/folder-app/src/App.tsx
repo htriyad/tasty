251f463399d042dc490800455d8d4f30fc01b70b
htriyad <htriyad846@gmail.com>
2026-05-23 14:01:57 +0600

    fix: import useHashLocation from wouter directly (not subpath)

--- artifacts/folder-app/src/App.tsx
+++ artifacts/folder-app/src/App.tsx
@@ -1,5 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
-import { useHashLocation } from "wouter/hash-location";
+import { Switch, Route, Router as WouterRouter, useHashLocation } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
